27a9f47dded9578b8e0ae808c518aaef91938607
kartiknoriya <148875248+kartiknoriya@users.noreply.github.com>
2026-01-15 13:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
+function Car (make , model){
+       this.make = make ;
+       this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function (){
+           return (`${this.make} ${this.model}`);
+}
+
+function SportsCar(make , model , topSpeed) {
+        Car.call(this , make , model)
+        this.topSpeed = topSpeed;
+        
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function(){
+    return (`${this.topSpeed}`)
+}
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call getMakeModel() to get the make and model of the car
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+
+// Call getTopSpeed() to get the top speed of the sports car
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
